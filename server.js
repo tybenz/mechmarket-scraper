46165d912418e69ab2a1d0e5724801d444368eb7
Tyler Benziger <tabenziger@gmail.com>
2015-03-13 10:06:28 -0700

    Cleaning up

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require( 'http' );
-
-var server = http.createServer( function( request, response ) {
-  response.writeHead( 200, { 'Content-Type': 'application/json' } );
-  response.write( '{"message": "Hello"}' );
-});
-
-server.listen( process.env.PORT || 3000 );
